1e1e3315e8eec72c20d8ecf5f3b81bbc7f4a02db
Phan Xuân Tuấn <prophan1171996@gmail.com>
2017-05-30 17:25:36 +0700

    modules footer

---
+++ footer.php
@@ -0,0 +1,11 @@
+<div class="xoa"></div>
+  <div id="footer">
+    <ul>
+	  <li><a href="#">Trang chủ</a></li>
+	  <li><a href="#">Tin tức</a></li>
+	  <li><a href="#">Bảo hành</a></li>					
+	  <li><a href="#">Download</a></li>
+      <li><a href="#">Liên hệ</a></li>
+    </ul>
+    <div class="copyright">Copyright: <a href="#">VNBlackBerry</a></div>
+  </div>
